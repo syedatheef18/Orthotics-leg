f1f0b4c6920425e37abf05f27de1dcff7086aea2
syedatheef18 <89172350+syedatheef18@users.noreply.github.com>
2023-11-21 12:09:33 +0530

    Orthotics-leg
    This helps to monitor the leg matrics

---
+++ app/src/main/java/com/example/orthotickfinal/PatientloginActivity.kt
@@ -0,0 +1,11 @@
+package com.example.orthotickfinal
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class PatientloginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.patientlogin)
+    }
+}
---
+++ app/src/main/java/com/example/orthotickfinal/SelectionpageActivity.kt
@@ -0,0 +1,11 @@
+package com.example.orthotickfinal
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SelectionpageActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.selectionpage)
+    }
+}
